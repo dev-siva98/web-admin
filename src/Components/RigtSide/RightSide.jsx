ddc3d708891e50261b3a27d2e1f37b6c94cc6a91
dev-siva98 <sivaprasadtppba@gmail.com>
2022-06-04 14:33:17 +0530

    edit

---
+++ src/Components/RigtSide/RightSide.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Updates from "../Updates/Updates";
+import "./RightSide.css";
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+      <h3>Updates</h3>
+        <Updates />
+    </div>
+  );
+};
+
+export default RightSide;
