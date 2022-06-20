e1b9926d775ccde88d98cc63545d7f06974cf9a8
dev-siva98 <sivaprasadtppba@gmail.com>
2022-06-20 15:26:05 +0530

    delivery status change

--- src/Components/DetailsModal/OrderDetails/OrderDetails.jsx
+++ src/Components/DetailsModal/OrderDetails/OrderDetails.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './OrderDetails.css'
 import axios from '../../../axios'
 import { Backdrop, Button } from '@mui/material'
 import { Close } from '@mui/icons-material'
+import Switch from "react-switch";
+import { useState } from 'react'
+
 
 
 
 function OrderDetails({ data, handleShow }) {
+
+    const [checked, setChecked] = useState(false)
+    const [input, setInput] = useState('Placed')
+
     const createdAt = new Date(data.createdAt)
         .toLocaleString('en-IN',
             { dateStyle: 'medium', timeStyle: 'short' })
@@ -14,19 +21,36 @@ function OrderDetails({ data, handleShow }) {
     const delivery = new Date(data.delivery)
         .toLocaleDateString('en-IN',
             { dateStyle: 'medium' })
-    const statusData = ['Pending', 'Placed', 'Delivered', 'Failed']
+
+    const statusData = ['Placed', 'Pending', 'Failed']
+
+    const handleToggle = () => {
+        setChecked(!checked)
+    }
+
+    useEffect(() => {
+        if (data.orderStatus === 'Delivered') setChecked(true)
+    }, [])
 
     const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleSave = () => { //set default incase options didn't change
+        console.log(input)
         axios({
             method: 'post',
             url: 'changeorderstatus',
             data: {
                 orderId: data.orderId,
-                value: e.target.value
+                value: checked ? 'Delivered' : input,
+                checked: checked
             }
-        }).catch(err => {
-            alert(err.message)
         })
+            .then(handleShow)
+            .catch(err => {
+                alert(err.message)
+            })
     }
 
     return (
@@ -35,7 +59,14 @@ function OrderDetails({ data, handleShow }) {
             open={true}
         >
             <div className='admin-order-details'>
-                <Close onClick={handleShow}
+                <Button
+                    onClick={handleSave}
+                    className='admin-order-details-save'
+                >
+                    Save
+                </Button>
+                <Close
+                    onClick={handleShow}
                     className='admin-order-details-button' />
                 <div className="admin-order-details-container">
                     <div className="admin-order-details-item">
@@ -66,23 +97,47 @@ function OrderDetails({ data, handleShow }) {
                         <span>Payment Mode</span><span>{data.paymentMode}</span>
                     </div>
                     <div className="admin-order-details-item">
-                        <span>Order Status</span>
+                        <span>Deilvery Status</span>
                         <span>
-                            <select name='orderStatus'
-                                onChange={(e) => {
-                                    handleChange(e)
-                                }}>
-                                <option value={data.orderStatus}>{data.orderStatus}</option>
-                                {
-                                    statusData.map((option, index) => {
-                                        if (option === data.orderStatus) return (null)
-                                        return <option value={option} key={index}>
-                                            {option}
-                                        </option>
-                                    })
-                                }
-                            </select>
+                            <Switch
+                                checked={checked}
+                                onChange={handleToggle}
+                                height={20}
+                                width={45}
+                                offColor='#FF0000'
+                                handleDiameter={22}
+                                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                                className='admin-order-details-switch'
+                            />
                         </span>
+                    </div>
+                    <div className="admin-order-details-item">
+                        <span>Order Status</span>
+                        {
+                            checked ? <span>Delivered</span> :
+                                <span>
+                                    <select name='orderStatus'
+                                        onChange={(e) => {
+                                            handleChange(e)
+                                        }}>
+                                        {
+                                            data.orderStatus !== 'Delivered' && <option value={data.orderStatus}>
+                                                {data.orderStatus}
+                                            </option>
+                                        }
+
+                                        {
+                                            statusData.map((option, index) => {
+                                                if (option === data.orderStatus) return (null)
+                                                return <option value={option} key={index}>
+                                                    {option}
+                                                </option>
+                                            })
+                                        }
+                                    </select>
+                                </span>
+                        }
                     </div>
                     <div className="admin-order-details-item">
                         <span>Payment Status</span><span>{data.paymentStatus}</span>
@@ -94,6 +149,7 @@ function OrderDetails({ data, handleShow }) {
                         <span>Write on Cake</span><span>{data.write}</span>
                     </div>
                 </div>
+
             </div>
         </Backdrop>
     )
